7b233ce4d87ed7070458eb37646d9a4615117ce1
palette3995 <sasadangon@icloud.com>
2024-01-23 00:08:38 +0900

    [fix] cssファイルの読み込み順を修正

--- src/Views/head.php
+++ src/Views/head.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Taskapp</title>
-    <link rel="stylesheet" href="../Style/common.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Style/common.css">
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script type="text/javascript" src="../js/common.js"></script>
   </head>
