919e3b1e64f69e966bd6dd93fd865eaabab7c3b2
palette3995 <sasadangon@icloud.com>
2024-01-23 00:06:56 +0900

    [add]仮ヘッダー・フッター実装

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -1,3 +1,8 @@
+    <footer>
+      <div class="footer">
+        <h1 class="pt-5">Task app</h1>
+      </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ src/Views/header.php
@@ -0,0 +1,11 @@
+<header class="mb-5">
+    <div class="header">
+        <div class="pt-4 d-flex flex-row-reverse">
+            <div class="header-link">Taskapp</div>
+            <div class="header-link">リンク</div>
+            <div class="header-link">リンク</div>
+            <div class="header-link">リンク</div>
+            <div class="header-link">リンク</div>
+        </div>
+    </div>
+</header>
